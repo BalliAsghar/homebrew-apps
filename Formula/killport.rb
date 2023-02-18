06283161bd29c5caad387653e5b93be477aa04cd
BalliAsghar <BalliAsghar@users.noreply.github.com>
2023-02-18 10:54:55 +0000

    Apply automatic changes

--- Formula/killport.rb
+++ Formula/killport.rb
@@ -1,8 +1,8 @@
 class Killport < Formula
   desc "Kill a process running on a specific port"
   homepage "https://github.com/BalliAsghar/killport"
-  url "https://github.com/BalliAsghar/killport/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "a156d6ef69eaf0355d70c704f4660ed53f9201b4"
+  url "https://github.com/BalliAsghar/killport/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "9acb69c258d10626cb250be43830e0c846177270"
   license "MIT"
 
   def install
