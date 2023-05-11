e55db9e24fe0b7eeecb09ead2ff157196c9b5ed7
Balli Asghar <ballihacker@outlook.com>
2023-05-11 15:00:52 +0100

    Create pc.rb

---
+++ Formula/pc.rb
@@ -0,0 +1,15 @@
+class pc < Formula
+  desc "partial clone Git repository"
+  homepage "https://github.com/BalliAsghar/partial-clone"
+  url ""
+  sha256 ""
+  license "MIT"
+
+  def install
+    bin.install "pc"
+  end
+
+  test do
+    system "false"
+  end
+end
