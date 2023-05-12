8af182b5353214b72728fe88c94cf1da8cfc56c9
Balli Asghar <ballihacker@outlook.com>
2023-05-12 14:31:43 +0100

    Update pc.rb

--- Formula/pc.rb
+++ Formula/pc.rb
@@ -1,8 +1,8 @@
 class pc < Formula
   desc "partial clone Git repository"
   homepage "https://github.com/BalliAsghar/partial-clone"
-  url ""
-  sha256 ""
+  url "https://github.com/BalliAsghar/partial-clone/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "2240c1da5949b270b10a8e1905c08b24a7beaef8d9230af0197291e0bfc835cc"
   license "MIT"
 
   def install
