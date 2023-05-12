ecfe3c0b5223d7fb983b71b048e999662ed3d2d3
Balli Asghar <ballihacker@outlook.com>
2023-05-12 14:43:48 +0100

    Update pc.rb

--- Formula/pc.rb
+++ Formula/pc.rb
@@ -1,4 +1,4 @@
-class PC < Formula
+class Pc < Formula
   desc "partial clone Git repository"
   homepage "https://github.com/BalliAsghar/partial-clone"
   url "https://github.com/BalliAsghar/partial-clone/archive/refs/tags/v1.0.0.tar.gz"
