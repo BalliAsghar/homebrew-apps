00f8b382883c17f484adf4016a1dce7142ce97d5
BalliAsghar <BalliAsghar@users.noreply.github.com>
2023-05-13 00:02:48 +0000

    Automated Change: partial-clone Updated

--- Formula/pc.rb
+++ Formula/pc.rb
@@ -1,8 +1,8 @@
 class Pc < Formula
   desc "partial clone Git repository"
   homepage "https://github.com/BalliAsghar/partial-clone"
-  url "https://github.com/BalliAsghar/partial-clone/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "2240c1da5949b270b10a8e1905c08b24a7beaef8d9230af0197291e0bfc835cc"
+  url "https://github.com/BalliAsghar/partial-clone/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "34e973b1b278ecb03d5379f18602990cbf6901b425ab8eb4204c6e60fac2069b"
   license "MIT"
 
   def install
