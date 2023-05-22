f8dd6f956fd8506abee821e1132737033d0b3f78
BalliAsghar <BalliAsghar@users.noreply.github.com>
2023-05-22 23:06:33 +0000

    Automated Change: Update Plex to ${VERSION}

--- Formula/plex.rb
+++ Formula/plex.rb
@@ -2,7 +2,7 @@ class Plex < Formula
   desc "Download Media from Plex"
   homepage "https://github.com/BalliAsghar/DownPlex"
   url "https://example.com/plex-1.0.0.tar.gz"
-  sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+  sha256 "eafee04534133aeffac35cdc65724a6491a4427196a42c230e6dc618812a99a0"
   license "MIT"
 
   depends_on "python@3.11"
