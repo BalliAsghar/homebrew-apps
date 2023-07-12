dd76f046ad3fe6a43d57a5c6e7d4484fc15ad948
BalliAsghar <BalliAsghar@users.noreply.github.com>
2023-07-12 00:28:52 +0000

    Automated Change: Update plex.rb to v2.0.7

--- Formula/plex.rb
+++ Formula/plex.rb
@@ -1,8 +1,8 @@
 class Plex < Formula
   desc "Download Media from Plex"
   homepage "https://github.com/BalliAsghar/DownPlex"
-  url "https://github.com/BalliAsghar/DownPlex/archive/refs/tags/v2.0.6.tar.gz"
-  sha256 "760f44e65df5ea0ef77975483dc2cc619f96f29cc3c67bc19aa768679501af89"
+  url "https://github.com/BalliAsghar/DownPlex/archive/refs/tags/v2.0.7.tar.gz"
+  sha256 "f24cf07747df50c7830c7721a7a1db738f8270cc5af17c62a9a804cbfda7199c"
   license "MIT"
 
   depends_on "python@3.11"
